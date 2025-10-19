db46b0a5deff452a4dcd531e3d0c2db43a59b875
Alexey <leskiy@yandex.ru>
2025-10-19 21:24:12 +0300

    third stage

--- core/request-service/src/main/java/ru/practicum/controller/ParticipationRequestPrivateController.java
+++ core/request-service/src/main/java/ru/practicum/controller/ParticipationRequestPrivateController.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.client.CollectorClient;
 import ru.practicum.dto.request.ParticipationRequestDto;
+import ru.practicum.ewm.stats.proto.ActionTypeProto;
+import ru.practicum.ewm.stats.proto.UserActionProto;
 import ru.practicum.service.ParticipationRequestService;
 
 import java.util.List;
@@ -16,6 +19,7 @@ import java.util.List;
 public class ParticipationRequestPrivateController {
 
     private final ParticipationRequestService requestService;
+    private final CollectorClient collectorClient;
 
     @GetMapping
     public List<ParticipationRequestDto> getRequests(@PathVariable Long userId) {
@@ -26,6 +30,11 @@ public class ParticipationRequestPrivateController {
     @ResponseStatus(HttpStatus.CREATED)
     public ParticipationRequestDto createRequest(@PathVariable Long userId,
                                                  @RequestParam Long eventId) {
+        collectorClient.collectUserAction(UserActionProto.newBuilder()
+                .setUserId(userId)
+                .setEventId(eventId)
+                .setActionType(ActionTypeProto.ACTION_REGISTER)
+                .build());
         return requestService.createRequest(userId, eventId);
     }
 
